c9c2d3f0eeb7ff92825cfd72cc71da3e4458e279
vonseyha <seyhavon22@gmail.com>
2021-02-04 12:38:29 +0700

    redesign profile

--- lib/screen/ProfileScreen/profilescreen.dart
+++ lib/screen/ProfileScreen/profilescreen.dart
@@ -1,5 +1,6 @@
+import 'package:KOOMPIACADEMY/widget/Form/reuse_textform_feild.dart';
 import 'package:flutter/material.dart';
-import 'package:circular_profile_avatar/circular_profile_avatar.dart';
+import 'package:gender_selector/gender_selector.dart';
 
 class ProfieUser extends StatefulWidget {
   @override
@@ -8,230 +9,140 @@ class ProfieUser extends StatefulWidget {
 
 class _ProfieUserState extends State<ProfieUser> {
   bool showPassword = false;
+  TextEditingController controller = new TextEditingController();
+  String selectedGender;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.0,vertical: 20.0),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            imageProfile(),
+            Text("ផ្នែកព័ត៌មានទូទៅ"),
+            Divider(height: 10),
+            SizedBox(height: 15),
+            ReuseTextFormField(
+              labelText: "គោត្តនាម និង នាមេ",
+              maxLength: 10,
+              maxLine: 1,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.black,
+              controller: controller,
+            ),
+            GenderSelector(
+  margin: EdgeInsets.only(left: 10, top: 30, right: 10, bottom: 10,),
+  selectedGender: Gender.FEMALE,
+  onChanged: (gender) async {
+
+    setState(() {
+      if(gender == Gender.FEMALE) {
+        selectedGender = "female";
+      } else {
+        selectedGender = "male";
+      }
+    });
+
+  },
+
+),
+            ReuseTextFormField(
+              labelText: "សាលារៀន",
+              maxLength: 10,
+              maxLine: 1,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.black,
+              controller: controller,
+            ),
+            ReuseTextFormField(
+              labelText: "ទីកន្លែង",
+              maxLength: 10,
+              maxLine: 1,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.black,
+              controller: controller,
+            ),
+            ReuseTextFormField(
+              labelText: "ប្រវិត្តសង្ខេប",
+              maxLength: 10,
+              maxLine: 1,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.black,
+              controller: controller,
+            )
+          ],
+        ),
+      )
+    );
+  }
+  
+  Widget imageProfile(){
+    return Center(
+      child: Stack(
         children: [
-          Expanded(
-            flex: 3,
-            child: Stack(
-              children: <Widget>[
-                ClipPath(
-                  clipper: _AppBarClipper(),
-                  child: Container(
-                    height: 250.0,
-                    decoration: BoxDecoration(color: Colors.black),
-                  ),
-                ),
-                Center(
-                  child: Stack(
-                    children: [
-                      Container(
-                        width: 110,
-                        height: 110,
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                width: 4,
-                                color:
-                                    Theme.of(context).scaffoldBackgroundColor),
-                            boxShadow: [
-                              BoxShadow(
-                                  spreadRadius: 2,
-                                  blurRadius: 10,
-                                  color: Colors.black.withOpacity(0.1),
-                                  offset: Offset(0, 10))
-                            ],
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: NetworkImage(
-                                  "https://images.pexels.com/photos/3307758/pexels-photo-3307758.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250",
-                                ))),
-                      ),
-                      Positioned(
-                          bottom: 0,
-                          right: 0,
-                          child: GestureDetector(
-                            onTap: (){
-                              print("Edit");
-                            },
-                              child: Container(
-                              height: 30,
-                              width: 30,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                border: Border.all(
-                                  width: 2,
-                                  color:
-                                      Theme.of(context).scaffoldBackgroundColor,
-                                ),
-                                color: Colors.black,
-                              ),
-                              child: Icon(
-                                Icons.edit,
-                                color: Colors.white,
-                                size: 20,
-                              ),
-                            ),
-                          )),
-                    ],
-                  ),
-                ),
-                SafeArea(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: Container(
-                          child: Center(
-                              child: Icon(
-                            Icons.arrow_back_ios,
-                            color: Colors.white,
-                          )),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(4),
-                            border: Border.all(
-                              color: Theme.of(context).scaffoldBackgroundColor,
-                            ),
-                          ),
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Container(
-                          child: Center(
-                              child: Icon(
-                            Icons.settings,
-                            color: Colors.white,
-                          )),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(4),
-                            border: Border.all(
-                              color: Theme.of(context).scaffoldBackgroundColor,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+          CircleAvatar(
+            radius: 60.0,
+            backgroundImage: AssetImage("images/splash22.png"),
+          ),
+          Positioned(
+            bottom: 10.0,
+            right: 10.0,
+            child: InkWell(
+              onTap: (){
+                print("Hele");
+                showModalBottomSheet(
+                  context: context, 
+                  builder: ((builder) => bottomSheet()),
+                );
+              },
+              child: Icon(
+                Icons.camera_alt,
+                color: Colors.blue,
+                size:23,
+              ),
             ),
           ),
-          Expanded(
-              flex: 7,
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      buildTextField("Full Name", "Dor Alex", false),
-                      buildTextField("Phone Number", "+885", false),
-                      buildTextField("E-mail", "alexd@gmail.com", false),
-                      buildTextField("History", "TLV, Israel", false),
-                      buildTextField("Old Password", "********", true),
-                      buildTextField("New Password", "********", true),
-                      buildTextField("Confirmed New Password", "********", true),
-                      SizedBox(
-                        height: 20,
-                      ),
-                    ],
-                  ),
-                ),
-              )
-            ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            OutlineButton(
-                              padding: EdgeInsets.symmetric(horizontal: 50),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20)),
-                              onPressed: () {},
-                              child: Text("CANCEL",
-                                  style: TextStyle(
-                                      fontSize: 14,
-                                      letterSpacing: 2.2,
-                                      color: Colors.black)),
-                            ),
-                            RaisedButton(
-                              onPressed: () {},
-                              color: Colors.black,
-                              padding: EdgeInsets.symmetric(horizontal: 50),
-                              elevation: 2,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20)),
-                              child: Text(
-                                "SAVE",
-                                style: TextStyle(
-                                    fontSize: 14,
-                                    letterSpacing: 2.2,
-                                    color: Colors.white),
-                              ),
-                            )
-                          ],
-                        ),
-              ),
-            )
         ],
       ),
     );
   }
 
-  Widget buildTextField(
-      String labelText, String placeholder, bool isPasswordTextField) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 35.0),
-      child: TextField(
-        obscureText: isPasswordTextField ? showPassword : false,
-        decoration: InputDecoration(
-            suffixIcon: isPasswordTextField
-                ? IconButton(
-                    onPressed: () {
-                      setState(() {
-                        showPassword = !showPassword;
-                      });
-                    },
-                    icon: Icon(
-                      Icons.remove_red_eye,
-                      color: Colors.grey,
-                    ),
-                  )
-                : null,
-            contentPadding: EdgeInsets.only(bottom: 3),
-            labelText: labelText,
-            floatingLabelBehavior: FloatingLabelBehavior.always,
-            hintText: placeholder,
-            hintStyle: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            )),
-      ),
+  Widget bottomSheet(){
+    return Container(
+        height: 100.0,
+        width: MediaQuery.of(context).size.width,
+        margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 20.0),
+        child: Column(
+          children: [
+            Text("ជ្រើសរូបភាពដំណាង",style: TextStyle(fontSize: 17)),
+            Divider(),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FlatButton.icon(
+                  onPressed: null, 
+                  icon: Icon(Icons.camera),
+                  label: Text("Camera"),
+              ),
+
+               FlatButton.icon(
+                  onPressed: null, 
+                  icon: Icon(Icons.image),
+                  label: Text("Gallery"),
+              )
+              
+              ],
+            )
+          ],
+          
+        ),
     );
   }
-}
 
-class _AppBarClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    double height = size.height - 80;
-    Path path = Path();
-    path.moveTo(0, height - 40);
-    path.quadraticBezierTo(size.width / 2, height, size.width, height - 40);
-    path.lineTo(size.width, 0);
-    path.lineTo(0, 0);
-    return path;
-  }
 
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+
+
 }
